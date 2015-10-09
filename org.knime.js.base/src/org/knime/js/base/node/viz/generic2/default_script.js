ec372ca0e2a3423ec8b7bc8304f6359cb3d45051
Christian Albrecht <christian.albrecht@knime.com>
2015-10-09 09:06:51 +0000

    Renaming org.knime.js.base to views

--- org.knime.js.base/src/org/knime/js/base/node/viz/generic2/default_script.js
+++
@@ -1,8 +0,0 @@
-var body = document.getElementsByTagName('body')[0];
-var html = "<h1>Data available?</h1>";
-if (knimeDataTable) {
-    html += '<div class="success">Data available. Node correctly configured. Table contains ' + knimeDataTable.getNumRows() + ' rows.</div>';
-} else {
-    html += '<div class="failure">No data available.</div>';
-}
-body.innerHTML = html;
