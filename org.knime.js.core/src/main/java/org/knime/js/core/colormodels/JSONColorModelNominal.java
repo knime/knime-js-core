87e8a78afe2f94d06fb6013cd0c7f0f4ec46eed7
Oleg Yasnev <oleg.yasnev@knime.com>
2017-02-04 00:20:51 +0100

    AP-6626: created classes for JSON color models

---
+++ org.knime.js.core/src/main/java/org/knime/js/core/colormodels/JSONColorModelNominal.java
@@ -0,0 +1,166 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   03.02.2017 (Oleg Yasnev): created
+ */
+package org.knime.js.core.colormodels;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ *
+ * @author Oleg Yasnev, KNIME GmbH, Berlin, Germany
+ */
+@JsonAutoDetect
+public class JSONColorModelNominal extends JSONColorModel {
+    private static final String CFG_LABELS = "labels";
+    private static final String CFG_COLORS = "colors";
+
+    private String[] m_labels;
+    private String[] m_colors;
+
+    /**
+     * @return the labels
+     */
+    public String[] getLabels() {
+        return m_labels;
+    }
+
+    /**
+     * @param labels the labels to set
+     */
+    public void setLabels(final String[] labels) {
+        this.m_labels = labels;
+    }
+
+    /**
+     * @return the colors
+     */
+    public String[] getColors() {
+        return m_colors;
+    }
+
+    /**
+     * @param colors the colors to set
+     */
+    public void setColors(final String[] colors) {
+        this.m_colors = colors;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @JsonIgnore
+    public void saveToNodeSettings(final NodeSettingsWO settings) {
+        super.saveToNodeSettings(settings);
+        settings.addStringArray(CFG_LABELS, getLabels());
+        settings.addStringArray(CFG_COLORS, getColors());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @JsonIgnore
+    public void loadFromNodeSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        super.loadFromNodeSettings(settings);
+        setLabels(settings.getStringArray(CFG_LABELS));
+        setColors(settings.getStringArray(CFG_COLORS));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @JsonIgnore
+    public void loadFromNodeSettingsInDialog(final NodeSettingsRO settings) {
+        super.loadFromNodeSettingsInDialog(settings);
+        setLabels(settings.getStringArray(CFG_LABELS, (String[])null));
+        setColors(settings.getStringArray(CFG_COLORS, (String[])null));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        JSONColorModelNominal other = (JSONColorModelNominal)obj;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(obj))
+                .append(m_labels, other.m_labels)
+                .append(m_colors, other.m_colors)
+                .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .appendSuper(super.hashCode())
+                .append(m_labels)
+                .append(m_colors)
+                .toHashCode();
+    }
+}
