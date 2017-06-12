9041250c565d0a886b1e143d1d3a9468a155e0f6
Christian Albrecht <christian.albrecht@knime.com>
2017-06-12 09:30:01 +0200

    AP-6786 changes to bridge
    AP-6786: Chromedriver wizard node view

--- org.knime.ext.seleniumdrivers.multios/src-js/selenium-knime-bridge.js
+++ org.knime.ext.seleniumdrivers.multios/src-js/selenium-knime-bridge.js
@@ -1,14 +1,23 @@
 seleniumKnimeBridge = function() {
 	
-	var FRAME_ID = 'knimeViewFrame';
+	const FRAME_ID = 'knimeViewFrame';
+	
+	const SIGNAL_NO_ACTION = 'NO_ACTION';
+	const SIGNAL_CLOSE_BUTTON_PRESSED = 'CLOSE_BUTTON_PRESSED';
+	const SIGNAL_RESET_BUTTON_PRESSED = 'RESET_BUTTON_PRESSED';
+	const SIGNAL_APPLY_BUTTON_PRESSED = 'APPLY_BUTTON_PRESSED';
+	const SIGNAL_CLOSE_WINDOW = 'CLOSE_WINDOW';
+	
+	var signalQueue = [];
 	
 	var bridge = {};
 	bridge.version = '1.0.0';
 	
-	bridge.laodJSONFile = function(url, callback) {
+	laodJSONFile = function(url, callback) {
 		var httpRequest = new XMLHttpRequest();
 		httpRequest.onreadystatechange = function() {
 			if (httpRequest.readyState === 4) {
+				// local requests leave status at 0
 				if (httpRequest.status === 200 || httpRequest.status === 0) {
 					var data = JSON.parse(httpRequest.responseText);
 					if (callback) callback(data);
@@ -34,7 +43,26 @@ seleniumKnimeBridge = function() {
 				eval(initCall);
 			}
 		});
-	}	
+	}
+	
+	signal = function(value) {
+		// enqueue and call signal, in most cases will be called immediately
+		signalQueue.push(value);
+		bridge.signalNext();
+	}
+	
+	bridge.signalNext = function() {
+		if (window.signal && signalQueue.length > 0) {
+			var toSignal = signalQueue.splice(0, 1);
+			window.signal(toSignal);
+		}
+	}
+	
+	window.addEventListener('beforeunload', function (event) {
+		if (window.signal) { 
+			window.signal(SIGNAL_CLOSE_WINDOW);
+		}
+	});
 		
 	return bridge;
 }();
