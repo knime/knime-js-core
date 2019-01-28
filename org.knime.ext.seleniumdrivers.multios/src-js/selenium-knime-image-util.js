3b217a4ec4bb0300ec659437ead45a4c24b01c0d
Christian Albrecht <christian.albrecht@knime.com>
2019-01-28 13:01:15 +0100

    AP-11276: fixing signal function erasure, linting
    AP-11276 (Communication between Chromium browser window and Java sometimes 'hangs')

--- org.knime.ext.seleniumdrivers.multios/src-js/selenium-knime-image-util.js
+++ org.knime.ext.seleniumdrivers.multios/src-js/selenium-knime-image-util.js
@@ -1,32 +1,33 @@
-knimeImageUtil = {};
+window.knimeImageUtil = {};
 
-knimeImageUtil.loadJSONFile = function(url, callback) {
-	var httpRequest = new XMLHttpRequest();
-	httpRequest.onreadystatechange = function() {
-		if (httpRequest.readyState === 4) {
-			// local requests leave status at 0
-			if (httpRequest.status === 200 || httpRequest.status === 0) {
-				var data = JSON.parse(httpRequest.responseText);
-				if (callback)
-					callback(data);
-			}
-		}
-	}
-	httpRequest.open('GET', url);
-	httpRequest.send();
-}
+window.knimeImageUtil.loadJSONFile = function (url, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) {
+            // local requests leave status at 0
+            if (httpRequest.status === 200 || httpRequest.status === 0) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) {
+                    callback(data);
+                }
+            }
+        }
+    };
+    httpRequest.open('GET', url);
+    httpRequest.send();
+};
 
-knimeImageUtil.loadView = function(repURL, valURL, initCall) {
-	knimeImageUtil.loadJSONFile(repURL, function(rep){
-		window.parsedRepresentation = rep;
-		if (window.parsedValue) {
-			eval(initCall);
-		}
-	});
-	knimeImageUtil.loadJSONFile(valURL, function(val){
-		window.parsedValue = val;
-		if (window.parsedRepresentation) {
-			eval(initCall);
-		}
-	});
-}
+window.knimeImageUtil.loadView = function (repURL, valURL, initCall) {
+    window.knimeImageUtil.loadJSONFile(repURL, function (rep) {
+        window.parsedRepresentation = rep;
+        if (window.parsedValue) {
+            eval(initCall);
+        }
+    });
+    window.knimeImageUtil.loadJSONFile(valURL, function (val) {
+        window.parsedValue = val;
+        if (window.parsedRepresentation) {
+            eval(initCall);
+        }
+    });
+};
