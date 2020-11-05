34ed106f37b75e6c3b6841575796601d71d0437d
Jakob Schröter <jakob.schroeter@knime.com>
2020-11-05 13:25:20 +0100

    AP-15477: fix eslint using webapps-common
    AP-15477 (Layout editor for configuration nodes)

--- org.knime.js.core/js-lib/knime/layoutEditor/.eslintrc.js
+++ org.knime.js.core/js-lib/knime/layoutEditor/.eslintrc.js
@@ -1,18 +1,13 @@
 module.exports = {
-    /*
-* Use
-* extends: ['./config/.eslintrc-nuxt.js']
-* for nuxt.js projects,
-* extends: ['./config/.eslintrc-vue.js']
-* for Vue.js projects,
-* extends: ['./config/.eslintrc-legacy.js']
-* for legacy views (KNIME AP, ES3), and
-* extends: ['./config/.eslintrc-base.js']
-* for other projects
-*/
-    extends: ['./config/.eslintrc-vue.js'],
+    extends: ['./webapps-common/lint/.eslintrc-vue.js'],
+    rules: {
+        'no-global-assign': ['error', {
+            // `require` is needed for the esm module
+            exceptions: ['require']
+        }]
+    },
     overrides: [{
-        files: ['{modules,plugins}/**'],
+        files: ['{modules,plugins,server,config}/**', '*.config.js'],
         env: {
             node: true
         }
