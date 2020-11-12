b4d0846cd7be759265a05ea6b8a4c33b065522a8
Jakob Schröter <jakob.schroeter@knime.com>
2020-11-12 17:01:25 +0100

    AP-15477: unit test improvements
    AP-15477 (Layout editor for configuration nodes)

--- org.knime.js.core/js-lib/knime/layoutEditor/test/unit/nodeSorter.store.test.js
+++ org.knime.js.core/js-lib/knime/layoutEditor/test/unit/nodeSorter.store.test.js
@@ -30,19 +30,19 @@ describe('store', () => {
         let newLayout = { rows: [] };
         store.commit('setLayout', newLayout);
         expect(store.state.layout).not.toBe(newLayout);
-        expect(store.state.layout).toEqual(newLayout);
+        expect(store.state.layout).toStrictEqual(newLayout);
     });
 
     it('allows setting nodes', () => {
         let newNodes = [{}];
         store.commit('setNodes', newNodes);
-        expect(store.state.nodes).toEqual(newNodes);
+        expect(store.state.nodes).toStrictEqual(newNodes);
     });
 
     it('allows updating and gettings rows', () => {
         let newRows = [{}];
         store.commit('updateRows', newRows);
-        expect(store.getters.getRows).toEqual(newRows);
+        expect(store.getters.getRows).toStrictEqual(newRows);
     });
 
 });
