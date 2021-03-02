caaa3c1efcd86022ad60b39161acdc460aeb87eb
Ben Laney <ben.laney@knime.com>
2021-03-02 14:28:11 +0100

    AP-16130: Formatting and modifier update
    AP-16130 (Sanitize HTML/JS Script if sanitize property is set)

--- org.knime.js.core/src/main/java/org/knime/js/core/StringSanitizationSerializer.java
+++ org.knime.js.core/src/main/java/org/knime/js/core/StringSanitizationSerializer.java
@@ -76,25 +76,24 @@ public class StringSanitizationSerializer extends StdSerializer<String> {
     /**
      * Custom String HTML sanitization serializer.
      *
-     * With the provided parameters, the class creates an internal sanitization policy to process string input
-     * and output strings which which are compliant with the configured policy for the purpose of safely rendering
-     * in a WebBrowser. The default policy is configured using OWASP standards and recommendations, but these can
-     * be overwritten with said parameters.
+     * With the provided parameters, the class creates an internal sanitization policy to process string input and
+     * output strings which which are compliant with the configured policy for the purpose of safely rendering in a
+     * WebBrowser. The default policy is configured using OWASP standards and recommendations, but these can be
+     * overwritten with said parameters.
      *
      * This may be desired whenever there is data from an unknown source being displayed in a visualization or web
      * application of some kind. No browser or data is 100% safe, but the parsing provided by this class can improve
      * data security in highly sensitive applications.
      *
-     * This process is not cost-free. Possible trade-offs include:
-     *      - *performance* parsing large data may be significantly slower than default serialization.
-     *      - *styling* output strings are often mangled and altered to prevent even the chance of malicious behavior
-     *        when delivered to the client.
-     *      - *functionality* some data may completely break the application as its format is sanitized beyond recognition.
+     * This process is not cost-free. Possible trade-offs include: - *performance* parsing large data may be
+     * significantly slower than default serialization. - *styling* output strings are often mangled and altered to
+     * prevent even the chance of malicious behavior when delivered to the client. - *functionality* some data may
+     * completely break the application as its format is sanitized beyond recognition.
      *
-     * For these reasons, this serialization method should be used sparingly (i.e. only on nodes which are expected
-     * to pose a potential risk or in certain applications where security concerns take precedent over the trade-offs
-     * previously mentioned). Finding the right balance for the application is essential to building modern, secure
-     * web applications.
+     * For these reasons, this serialization method should be used sparingly (i.e. only on nodes which are expected to
+     * pose a potential risk or in certain applications where security concerns take precedent over the trade-offs
+     * previously mentioned). Finding the right balance for the application is essential to building modern, secure web
+     * applications.
      *
      * @param allowElem - HTML element tags which should be allowed in sanitized output (overrides OWASP suggested)
      * @param allowAttr - HTML attributes which should be allowed in sanitized output (overrides OWASP suggested)
@@ -109,15 +108,16 @@ public class StringSanitizationSerializer extends StdSerializer<String> {
      * {@inheritDoc}
      */
     @Override
-    public void serialize(final String value, final JsonGenerator gen, final SerializerProvider provider) throws IOException {
+    public void serialize(final String value, final JsonGenerator gen, final SerializerProvider provider)
+        throws IOException {
         gen.writeString(sanitize(value));
     }
 
     /**
-     * Use the member {@link HtmlPolicyBuilder} to attempt to sanitize the input string. Sanitization is greedy
-     * and if pre-processing of strings is possible to reduce or remove the risk of Cross-Site-Script injection,
-     * then it's recommended to try this approach (as functionality or aesthetic details may be lost during the
-     * serialization process).
+     * Use the member {@link HtmlPolicyBuilder} to attempt to sanitize the input string. Sanitization is greedy and if
+     * pre-processing of strings is possible to reduce or remove the risk of Cross-Site-Script injection, then it's
+     * recommended to try this approach (as functionality or aesthetic details may be lost during the serialization
+     * process).
      *
      * @param dirtyString
      * @return cleanString
@@ -125,9 +125,8 @@ public class StringSanitizationSerializer extends StdSerializer<String> {
     private String sanitize(final String dirtyString) {
         StringBuilder sanitizedValueBuilder = new StringBuilder();
 
-        HtmlStreamRenderer streamRenderer = HtmlStreamRenderer.create(sanitizedValueBuilder, stringViolation ->
-            LOGGER.debug("Sanitization policy violation detected: " + stringViolation)
-        );
+        HtmlStreamRenderer streamRenderer = HtmlStreamRenderer.create(sanitizedValueBuilder,
+            stringViolation -> LOGGER.debug("Sanitization policy violation detected: " + stringViolation));
 
         HtmlSanitizer.sanitize(dirtyString, m_policyBuilder.build(streamRenderer));
 
@@ -147,7 +146,8 @@ public class StringSanitizationSerializer extends StdSerializer<String> {
      *
      * @return built policy
      */
-    private static HtmlPolicyBuilder createPolicy(final String allowElem, final String allowAttr, final boolean allowStyles) {
+    private static HtmlPolicyBuilder createPolicy(final String allowElem, final String allowAttr,
+        final boolean allowStyles) {
         HtmlPolicyBuilder policyBuilder = new HtmlPolicyBuilder();
 
         if (StringUtils.isNotEmpty(allowElem)) {
