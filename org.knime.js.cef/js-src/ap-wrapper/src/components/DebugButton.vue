8e5af9a81ab172a2922a8c2c9c94758c7abb4a39
Daniel Bogenrieder <daniel.bogenrieder@knime.com>
2021-09-09 16:46:44 +0200

    NXT-675: Now opens the debugger window directly instead of the overview page
    NXT-675 (Add link to debugging console (classic AP))

--- org.knime.js.cef/js-src/ap-wrapper/src/components/DebugButton.vue
+++ org.knime.js.cef/js-src/ap-wrapper/src/components/DebugButton.vue
@@ -4,6 +4,38 @@ import InspectorIcon from '~/webapps-common/ui/assets/img/icons/code-html.svg?in
 export default {
     components: {
         InspectorIcon
+    },
+    data() {
+        return {
+            debugPage: ''
+        };
+    },
+    computed: {
+        // Composed URL to either open the correct debugger or the overview page
+        debugUrl() { return `http://localhost:8888${this.debugPage}`; }
+    },
+    mounted() {
+        // Code taken from cef debugger
+        let tabsListRequest = new XMLHttpRequest();
+        tabsListRequest.open("GET", `http://localhost:8888/json/list?t=${new Date().getTime()}`, true);
+        tabsListRequest.onreadystatechange = this.onReady;
+        tabsListRequest.send();
+    },
+    methods: {
+        // If more than one debugger is possible redirect to the debugger list, otherwise open the debugger directly
+        onReady(e) {
+            if (e.target.readyState === 4 && e.target.status === 200) {
+                let responseJSON;
+                if (e.target.response !== null) {
+                    responseJSON = JSON.parse(e.target.response);
+                }
+                if (responseJSON.length > 1) {
+                    this.debugPage = '';
+                } else {
+                    this.debugPage = responseJSON[0].devtoolsFrontendUrl;
+                }
+            }
+        }
     }
 };
 </script>
@@ -11,7 +43,7 @@ export default {
 <template>
   <div class="container">
     <a
-      href="http://localhost:8888"
+      :href="debugUrl"
       target="_blank"
       class="float"
     >
