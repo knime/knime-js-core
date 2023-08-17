249ab6e6b8102ff65faf7ec39f4eb8da0a02f946
Martin Horn <martin.horn@knime.com>
2023-08-17 15:57:16 +0200

    UIEXT-1131: Auxiliary interface introduced
    ... to be able to set a base-url required by the CEF middleware to be
    able to intercept those requests.
    
    UIEXT-1131 (File Download Widget doesn't work on CEF browser)

---
+++ org.knime.js.core/src/main/java/org/knime/js/core/BaseUrlSetter.java
@@ -0,0 +1,70 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Aug 17, 2023 (hornm): created
+ */
+package org.knime.js.core;
+
+/**
+ * The purpose of this interface is to solve UIEXT-1131 (no file-chooser opens when clicking a file-url in CEF).
+ * Implemented by the {@code FileDownloadWidgetRepresentation} such that the CEF-based view can set the base-url. This
+ * base-url is combined with the path of the file to download in order to build the download link which in turn then can
+ * be intercepted by the CEF middleware.
+ *
+ * If no base-url is set, the file download link will be a file-url (file://...).
+ *
+ * @author Martin Horn, KNIME GmbH, Konstanz, Germany
+ *
+ * @since 5.2
+ */
+public interface BaseUrlSetter {
+
+    /**
+     * @param baseUrl
+     */
+    void setBaseUrl(String baseUrl);
+
+}
