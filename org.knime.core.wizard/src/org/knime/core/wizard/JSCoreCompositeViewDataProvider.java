6cc3e8f630739320c0fe64d57f4995c8a38900e5
Tobias Kampmann <tobias.kampmann@tngtech.com>
2025-02-13 16:53:00 +0100

    NXT-3307: Add reexecution function
    NXT-3307 (ComponentView: new endpoint for reexecution interaction)

--- org.knime.core.wizard/src/org/knime/core/wizard/JSCoreCompositeViewDataProvider.java
+++ org.knime.core.wizard/src/org/knime/core/wizard/JSCoreCompositeViewDataProvider.java
@@ -50,6 +50,7 @@ package org.knime.core.wizard;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.knime.core.node.web.WebViewContent;
@@ -86,6 +87,16 @@ public class JSCoreCompositeViewDataProvider implements CompositeViewDataProvide
         // WebViewContent should have a 'saveToStream(OutputStream)'-method
         // right now the returning will be json, but what if not anymore?
         return ((ByteArrayOutputStream)webViewContent.saveToStream()).toString("UTF-8");
+    }
+
+    @Override
+    public void triggerComponentReexecution(final SubNodeContainer snc, final String resetNodeIdSuffix,
+        final Map<String, String> stateUpdates, final Function<NativeNodeContainer, NodeViewEnt> createNodeViewEnt)
+        throws IOException {
+
+        var model = new SubnodeViewableModel(snc, snc.getName());
+        model.createReexecutionService((WizardPageCreationHelper)createNodeViewEnt::apply)
+            .reexecutePage(resetNodeIdSuffix, stateUpdates);
     }
 
 }
